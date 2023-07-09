d5453e91b99d5058381d47196f2cf1d3de1760c1
William-Dores <williamdores.s132@outlook.com>
2023-07-09 01:53:43 -0300

    FindById impementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,23 @@
 package application;
 
-import java.util.Date;
+import java.util.Scanner;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("INFORME O ID QUE DESEJA CONSULTAR NO DB");
+		int id = sc.nextInt();
 		
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(id);
+	
 		System.out.println(seller);
 	}
 
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,16 @@
 package model.dao;
 
+import java.util.List;
+import model.entities.Department;
+
 public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	
+	//AQUI RETORNA O OBJ no DEPARTAMENTO COM ESSE ID
+	Department findById(Integer id);
+	List<Department> findAll();
 
 }
